c106a6499fd2783588b1fc83a0551d58346e01c5
Al <al-mac@MacBook-Pro-de-Al.local>
2018-09-11 12:41:58 -0600

    First commit for the todo api

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,51 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
+    if(err){
+     return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed: false
+    // }, (err, result)=>{
+    //     if(err){
+    //         return console.log('Unable to insert todo', err);
+    //     }
+    //
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+   // db.collection('Users').insertOne({
+   //
+   //     name: 'Boshka',
+   //     age: 27,
+   //     location: 'Great land'
+   // },(err, result)=>{
+   //     if(err){
+   //         return console.log('Unable to insert user', err);
+   //     }
+   //
+   //     console.log(result.ops[0]._id.getTimestamp());
+   // });
+
+   db.collection('Boshka').insertOne({
+       name: 'Boshka',
+       food: 'Tuna and Crispy bones',
+       nemesis: 'Felix',
+       hobbies: 'hunting lizzards'
+   }, (err, result)=>{
+       if(err){
+           return console.log('Unable to insert user');
+       }
+       console.log(result.ops[0]);
+   });
+
+    db.close();
+});
