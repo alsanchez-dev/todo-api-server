befe3e14685994b60307734e1147215459ffe7a0
Al <al-mac@MacBook-Pro-de-Al.local>
2018-09-11 12:41:58 -0600

    Added additional connection to local mongoDB database with mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://udemycourse1:b0shk4b1sd3n4@ds163842.mlab.com:63842/udemy-todo');
+mongoose.connect('mongodb://udemycourse1:b0shk4b1sd3n4@ds163842.mlab.com:63842/udemy-todo' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
